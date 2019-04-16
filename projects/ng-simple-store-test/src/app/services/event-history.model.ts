7c935070c6cd4900cb01cf920dbb03a81b1d7e60
LinhTH <truonghoanglinhk55bkhn@gmail.com>
2019-04-16 07:15:34 +0700

    feat(test): Create EventHistory Model

--- projects/ng-simple-store-test/src/app/services/event-history.model.ts
+++ projects/ng-simple-store-test/src/app/services/event-history.model.ts
@@ -1,4 +1,27 @@
+import * as faker from 'faker';
 
 export class EventHistory {
-    constructor(title: string) {}
+  constructor(
+    public eventId: string,
+    public type: string,
+    public createdAt: string,
+    public title: string,
+    public partnerIds: string[],
+    public contractIds: string[],
+    public caseNumbers: string[]
+  ) {}
+}
+
+export class EventHistoryBuilder {
+  static build(): EventHistory {
+    return new EventHistory(
+      faker.finance.account(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      [faker.lorem.sentence()],
+      [faker.lorem.sentence()],
+      [faker.lorem.sentence()]
+    );
+  }
 }
