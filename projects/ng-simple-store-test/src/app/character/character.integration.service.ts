7cd405d2eb6887a6bc09dc5f354198d7508fe2a6
Linh Truong <thlinh8392@outlook.com>
2021-09-10 14:09:54 +0700

    fix: correct the type of retrieved response

--- projects/ng-simple-store-test/src/app/character/character.integration.service.ts
+++ projects/ng-simple-store-test/src/app/character/character.integration.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { retry } from 'rxjs/operators';
+import { map, retry } from 'rxjs/operators';
 import { Character } from './character.model';
 
 const BASE_URL = 'https://breakingbadapi.com/api/characters';
@@ -13,6 +13,9 @@ export class CharacterIntegrationService {
     constructor(private http$: HttpClient) {}
 
     public getCharacter(characterId: string): Observable<Character> {
-        return this.http$.get<Character>(`${BASE_URL}/${characterId}`).pipe(retry(3));
+        return this.http$.get<Character>(`${BASE_URL}/${characterId}`).pipe(
+            retry(3),
+            map((response) => response?.[0])
+        );
     }
 }
