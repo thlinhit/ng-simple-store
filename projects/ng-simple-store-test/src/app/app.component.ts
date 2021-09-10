1d5f2579227d371c9ee4a8af04ed68b3763f7c3d
Linh Truong <thlinh8392@outlook.com>
2021-09-10 14:10:20 +0700

    feat: add example

--- projects/ng-simple-store-test/src/app/app.component.ts
+++ projects/ng-simple-store-test/src/app/app.component.ts
@@ -1,11 +1,28 @@
-import { Store } from './../../../ng-simple-store/src/lib/store';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Character, CharacterSelector, CharacterStoreService } from './character';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'ng-simple-store-test';
+export class AppComponent implements OnInit {
+    title = 'ng-simple-store-test';
+    characters$: Observable<Character[]> = this.characterSelector.selectAll$();
+    characterId: string;
+    isLoading$: Observable<boolean>;
+
+    constructor(private characterStoreService: CharacterStoreService, private characterSelector: CharacterSelector) {}
+
+    ngOnInit(): void {
+        this.characterStoreService.load(['1', '2']);
+    }
+
+    loadCharacter(): void {
+        if (this.characterId) {
+            this.characterStoreService.load([this.characterId]);
+            this.isLoading$ = this.characterSelector.isLoading$(this.characterId);
+        }
+    }
 }
